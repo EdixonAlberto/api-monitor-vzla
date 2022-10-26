7c098a7320db571705c40d3af49a8137a7be2f4f
Edixon Piña <edixonalbertto@gmail.com>
2022-10-25 23:04:58 -0400

    install typegoose, remove files, edit tsconfig
    - install libraries
    - add alias
    - create file ignore to prettier

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(AppModule.port)
+
+  new Logger('NestApplication').log(`Server listening in port ${AppModule.port}`)
 }
 bootstrap()
